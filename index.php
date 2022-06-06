4c9749e2e10d7b4105f66c11af43f1f762dff6e5
agostinopique <ago.pique@yahoo.it>
2022-06-06 17:12:00 +0200

    Aggiunto paragrafo con conteggio dei caratteri;

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+$loremParagraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores libero ut debitis minima expedita. Veritatis, reiciendis quibusdam quae fugit consequuntur labore ullam hic inventore itaque eveniet illum, reprehenderit, soluta suscipit!";
 
+$paragraphLength = strlen(trim($loremParagraph));
 
 ?>
-
+<!-- Aggiunto paragrafo e conteggio dei caratteri; -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,5 +15,7 @@
 </head>
 <body>
     <h1>Badwords</h1>
+    <p><?php echo $loremParagraph ?></p>
+    <p>Il paragrafo é lungo <?php echo $paragraphLength?> caratteri.</p>
 </body>
 </html>
