f696b934e24dce113760a9d3b84fff62a4199308
agostinopique <ago.pique@yahoo.it>
2022-06-07 10:12:11 +0200

    Aggiunta alternativa del conteggio dei caratteri direttamente in HTML;

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-$loremParagraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores libero ut debitis minima expedita. Veritatis, reiciendis quibusdam quae fugit consequuntur labore ullam hic inventore itaque eveniet illum, reprehenderit, soluta suscipit!";
+$loremParagraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, ea nostrum laborum ad eligendi aliquid necessitatibus cumque accusamus dolor, maxime tempore officia nisi recusandae expedita corporis sunt eius error dolorem commodi. Ipsam repellendus aliquid tempora sunt officiis, perspiciatis atque corporis impedit iste doloribus, natus praesentium. Id soluta, aut officiis facere, deserunt eaque, ut impedit possimus fuga odio neque. Ipsam porro, eos dolorum, pariatur error, adipisci fugiat hic quo corrupti ea harum quisquam doloribus labore doloremque facilis. Consequuntur ea eligendi reiciendis architecto? Mollitia tempora delectus accusamus nesciunt aut, sapiente repudiandae rem commodi iste qui quo molestiae non corporis nostrum. Id, corporis!";
 
 $noSpaceParagraph = str_replace(" ", "", $loremParagraph);
-$paragraphLength = strlen($noSpaceParagraph);
+// CONTEGGIO DEI CARATTERI AGGIUNTO DIRETTAMENTE IN HTML
+// $paragraphLength = strlen($noSpaceParagraph);
 // $paragraphLength = strlen($loremParagraph);
 
 $censoredParagraph = str_ireplace($_GET["badword"], "***", $loremParagraph);
 $censNoSpaceLength = str_replace(" ", "", $censoredParagraph);
-$censParaLength = strlen($censNoSpaceLength);
+// CONTEGGIO DEI CARATTERI AGGIUNTO DIRETTAMENTE IN HTML
+// $censParaLength = strlen($censNoSpaceLength);
 
 ?>
 
@@ -22,10 +24,16 @@ $censParaLength = strlen($censNoSpaceLength);
 <body>
     <h1>Badwords</h1>
 
-    <p><?php echo $loremParagraph ?></p>
-    <p>Il paragrafo é lungo <?php echo $paragraphLength?> caratteri.</p>
+    <h2>Il paragrafo é il seguente:</h2>
 
+    <p><?php echo $loremParagraph ?></p>
+    <p>Il paragrafo é lungo <?php echo strlen($noSpaceParagraph)?> caratteri.</p>
+
+    <hr>
+    
+    <h2>Questo é il paragrafo censurato:</h2>
+    
     <p><?php echo $censoredParagraph ?></p>
-    <p>Il paragrafo é lungo <?php echo $censParaLength?> caratteri.</p>
+    <p>Il paragrafo é lungo <?php echo strlen($censNoSpaceLength)?> caratteri.</p>
 </body>
 </html>
